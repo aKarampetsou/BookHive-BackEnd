49ba009f658ad2963de381e44db1d4982aed378e
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-10-01 01:49:08 +0300

    Backend files
    Adding Backend files (springboot)

---
+++ BookHive/src/test/java/com/BookHive/BookHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
