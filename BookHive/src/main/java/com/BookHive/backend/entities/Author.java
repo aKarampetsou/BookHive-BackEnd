f8b148521d0efd1ef70ecd1f2dfd147dcf77a1bc
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-10-03 13:41:23 +0300

    created entities

---
+++ BookHive/src/main/java/com/BookHive/backend/entities/Author.java
@@ -0,0 +1,52 @@
+package com.BookHive.backend.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    // Constructors
+    public Author() {
+    }
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
---
+++ BookHive/src/main/java/com/BookHive/backend/entities/Book.java
@@ -0,0 +1,58 @@
+package com.BookHive.backend.entities;
+
+//import com.BookHive.backend.entities.Author;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    
+
+    // Constructors
+    public Book() {
+    }
+
+    public Book(String title, Author author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
