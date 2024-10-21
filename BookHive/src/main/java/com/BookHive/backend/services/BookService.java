d178c62b6f0d7c5026a5ae5a8f481998a00c7df0
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-10-21 21:45:48 +0300

    Improved logic for book creation and association with authors

--- BookHive/src/main/java/com/BookHive/backend/services/BookService.java
+++ BookHive/src/main/java/com/BookHive/backend/services/BookService.java
@@ -1,6 +1,8 @@
 package com.BookHive.backend.services;
 
+import com.BookHive.backend.entities.Author;
 import com.BookHive.backend.entities.Book;
+import com.BookHive.backend.repositories.AuthorRepository;
 import com.BookHive.backend.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,21 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private AuthorRepository authorRepository; // Κάνε inject το AuthorRepository εδώ
+
     // Δημιουργία βιβλίου
     public Book createBook(Book book) {
-        return bookRepository.save(book);
+        Optional<Author> authorOptional = authorRepository.findById(book.getAuthor().getId());
+        if (authorOptional.isPresent()) {
+            book.setAuthor(authorOptional.get());  
+            return bookRepository.save(book);  // Αποθήκευση του βιβλίου
+        } else {
+            throw new RuntimeException("Author not found with id: " + book.getAuthor().getId());
+        }
     }
-
+    
+    
     // Εύρεση βιβλίου με βάση το id
     public Optional<Book> getBookById(Long id) {
         return bookRepository.findById(id);
