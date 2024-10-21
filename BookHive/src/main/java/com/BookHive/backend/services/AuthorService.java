6a38abd392b953b926987282451dc6e658d1bbed
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-10-21 21:42:27 +0300

    Improved book management as well as author searches by name

--- BookHive/src/main/java/com/BookHive/backend/services/AuthorService.java
+++ BookHive/src/main/java/com/BookHive/backend/services/AuthorService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import com.BookHive.backend.entities.Author;
 import com.BookHive.backend.entities.Book;
 import com.BookHive.backend.repositories.AuthorRepository;
-import com.BookHive.backend.repositories.BookRepository; // Εισαγωγή του BookRepository
+import com.BookHive.backend.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,43 +16,60 @@ public class AuthorService {
     private AuthorRepository authorRepository;
 
     @Autowired
-    private BookRepository bookRepository;  // Δήλωση και αυτόματο dependency injection για το BookRepository
+    private BookRepository bookRepository;
 
+    // Επιστρέφει όλους τους συγγραφείς από τη βάση δεδομένων
     public List<Author> getAllAuthors() {
         return authorRepository.findAll();
     }
 
+    // Επιστρέφει έναν συγγραφέα με βάση το ID
     public Optional<Author> getAuthorById(Long id) {
         return authorRepository.findById(id);
     }
 
+    // Δημιουργεί έναν νέο συγγραφέα στη βάση δεδομένων
     public Author createAuthor(Author author) {
         return authorRepository.save(author);
     }
 
+    // Διαγράφει έναν συγγραφέα με βάση το ID
     public void deleteAuthor(Long id) {
         authorRepository.deleteById(id);
     }
 
-    // Μέθοδος για την ενημέρωση υπάρχοντος συγγραφέα
+    // Ενημερώνει τα στοιχεία ενός συγγραφέα
     public Author updateAuthor(Long id, Author authorDetails) {
-        Optional<Author> optionalAuthor = authorRepository.findById(id); // Εύρεση του συγγραφέα με το συγκεκριμένο ID
+        Optional<Author> optionalAuthor = authorRepository.findById(id);
 
         if (optionalAuthor.isPresent()) {
-            Author author = optionalAuthor.get(); // Λήψη του υπάρχοντος συγγραφέα
-
-            // Ενημέρωση των πεδίων του συγγραφέα
+            Author author = optionalAuthor.get();
             author.setName(authorDetails.getName());
             author.setSurname(authorDetails.getSurname());
-
-            // Αποθήκευση του ενημερωμένου συγγραφέα στη βάση δεδομένων
             return authorRepository.save(author);
         } 
-        return null; // Επιστροφή null αν ο συγγραφέας δεν βρεθεί
+        return null;  // Επιστρέφει null αν δεν βρεθεί ο συγγραφέας
     }
 
-    // Νέα μέθοδος για την εύρεση των βιβλίων ενός συγγραφέα
+    // Επιστρέφει τα βιβλία ενός συγγραφέα
     public List<Book> getBooksByAuthor(Long authorId) {
-        return bookRepository.findByAuthorId(authorId);  // Χρησιμοποιούμε το bookRepository για να βρούμε τα βιβλία του συγγραφέα
+        return bookRepository.findByAuthorId(authorId);
+    }
+
+    // Προσθέτει ένα βιβλίο σε έναν συγγραφέα
+    public Book addBookToAuthor(Long authorId, Book book) {
+        Optional<Author> authorOptional = authorRepository.findById(authorId);
+        if (authorOptional.isPresent()) {
+            Author author = authorOptional.get();
+            book.setAuthor(author);
+            return bookRepository.save(book);
+        } else {
+            throw new RuntimeException("Author not found with id: " + authorId);
+        }
+    }
+
+     // Μέθοδος για αναζήτηση συγγραφέα με βάση το όνομα καιτο επώνυμο
+     public Author getAuthorByNameAndSurname(String name, String surname) {
+        return authorRepository.findByNameAndSurname(name, surname);
     }
 }
