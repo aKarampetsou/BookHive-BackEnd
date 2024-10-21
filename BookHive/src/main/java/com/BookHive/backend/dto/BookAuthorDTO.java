11854f7d2cdbf911d90d520886e9d91fd947e45d
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-10-21 21:40:30 +0300

    Created DTO for book and author data transfer

---
+++ BookHive/src/main/java/com/BookHive/backend/dto/BookAuthorDTO.java
@@ -0,0 +1,41 @@
+package com.BookHive.backend.dto;
+
+public class BookAuthorDTO {
+    private String title;
+    private String isbn;
+    private String authorName;
+    private String authorSurname;
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorSurname() {
+        return authorSurname;
+    }
+
+    public void setAuthorSurname(String authorSurname) {
+        this.authorSurname = authorSurname;
+    }
+}
