669a6b2827a5980bfd9de11cae038982599cf1f4
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-10-23 16:13:18 +0300

    Added new query for searching authors by name or surname

--- BookHive/src/main/java/com/BookHive/backend/repositories/AuthorRepository.java
+++ BookHive/src/main/java/com/BookHive/backend/repositories/AuthorRepository.java
@@ -1,12 +1,8 @@
-
 package com.BookHive.backend.repositories;
 
 import com.BookHive.backend.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> { //extends entity, type of primary key=long
-
-    // Να θυμηθώ να προσθέσω custom queries εδώ αν χρειαστεί
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Author findByNameAndSurname(String name, String surname);
 }
