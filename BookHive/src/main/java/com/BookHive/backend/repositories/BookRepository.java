027fe5f172ab08d2823086b54bde0c08b9a3416d
aKarampetsou <anastasiakarampetsou@gmail.com>
2024-11-05 19:35:09 +0200

    added custom query to check for duplicates

--- BookHive/src/main/java/com/BookHive/backend/repositories/BookRepository.java
+++ BookHive/src/main/java/com/BookHive/backend/repositories/BookRepository.java
@@ -4,10 +4,16 @@ import java.util.List;
 import com.BookHive.backend.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    
+
     // Μέθοδος για την εύρεση βιβλίων με βάση το author_id
     List<Book> findByAuthorId(Long authorId);
+
+    // Μέθοδος για έλεγχο αν υπάρχει βιβλίο με συγκεκριμένα στοιχεία
+    @Query("SELECT COUNT(b) > 0 FROM Book b WHERE b.title = :title AND b.author.name = :authorName AND b.author.surname = :authorSurname AND b.isbn = :isbn")
+    boolean existsByTitleAndAuthorAndIsbn(@Param("title") String title, @Param("authorName") String authorName, @Param("authorSurname") String authorSurname, @Param("isbn") String isbn);
 }
